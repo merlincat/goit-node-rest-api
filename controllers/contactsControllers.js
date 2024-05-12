b4916fee68e134cdbedbd168166ef2a18243dd20
NataOz <merlincat.no@gmail.com>
2024-05-12 23:56:56 +0300

    add hw02, all functions

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,90 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactInfo,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(HttpError(500, "Internal server error"));
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await getContactById(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedContact = await removeContact(id);
+    if (!deletedContact) {
+      throw HttpError(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  const { error } = createContactSchema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await addContact(name, email, phone);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  const { error } = updateContactSchema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+  try {
+    const { id } = req.params;
+    const updatedContact = await updateContactInfo(id, req.body);
+    if (!updatedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// {
+//     "name":"Dan",
+//     "email":"rtggg@email.com",
+//     "phone":"(992) 914-3792"
+// }
+
+//  {
+//     "id": "rsKkOQUi80UsgVPCcLZZW",
+//     "name": "Alec Howard",
+//     "email": "Donec.elementum@scelerisquescelerisquedui.net",
+//     "phone": "(748) 206-2688"
+//   }
