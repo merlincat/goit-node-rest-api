7121f015b97904d90f7714183feaf0813422b7a2
NataOz <merlincat.no@gmail.com>
2024-05-17 13:43:24 +0300

    add initial files for HW

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,78 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactInfo,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await listContacts();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(HttpError(500, "Internal server error"));
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await getContactById(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedContact = await removeContact(id);
+    if (!deletedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(deletedContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  const { error } = createContactSchema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+  try {
+    const { name, email, phone } = req.body;
+    const newContact = await addContact(name, email, phone);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  const { error } = updateContactSchema.validate(req.body);
+  if (error) {
+    return next(HttpError(400, error.message));
+  }
+  try {
+    const { id } = req.params;
+    const updatedContact = await updateContactInfo(id, req.body);
+    if (!updatedContact) {
+      throw HttpError(404);
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,32 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string()
 
-})
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/)
+    .messages({
+      "string.pattern.base":
+        "Please use correct format for phone number (XXX) XXX-XXXX",
+    })
+    .required(),
+});
 
 export const updateContactSchema = Joi.object({
-
+  name: Joi.string().min(2).max(30),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string()
+    .pattern(/^\(\d{3}\)\s\d{3}-\d{4}$/)
+    .messages({
+      "string.pattern.base":
+        "Please use correct format for phone number (XXX) XXX-XXXX",
+    }),
 })
+  .min(1)
+  .messages({ "object.min": "Body must have at least one field" });
