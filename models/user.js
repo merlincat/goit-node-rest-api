7e8fd02ace8195077600bc13a1d05a3e947b61ff
NataOz <merlincat.no@gmail.com>
2024-05-26 02:34:59 +0300

    cut name from schema

--- models/user.js
+++ models/user.js
@@ -7,9 +7,6 @@ const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const { Schema, model } = mongoose;
 const userSchema = new Schema(
   {
-    name: {
-      type: String,
-    },
     email: {
       type: String,
       required: [true, "Email is required"],
@@ -39,7 +36,6 @@ export const User = model("user", userSchema);
 // joi
 
 export const registerSchema = Joi.object({
-  name: Joi.string().required(),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
